0c1004943b9a4f117ae0bdf4c6f3eefff81ff1b5
114774-create <114774@csps.tn.edu.tw>
2026-03-16 10:47:11 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing");
-}
-
 export default defineConfig({
   out: "./drizzle",
-  schema: "./drizzle/schema.ts", // 如果您的 schema 在別處請調整路徑
+  schema: "./drizzle/schema.ts", // 指向您的 schema 檔案
   dialect: "mysql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
