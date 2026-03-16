e79e4b7302476cb5f0a373347aed7eea7721ceff
114774-create <114774@csps.tn.edu.tw>
2026-03-16 10:54:53 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./drizzle/schema.ts", // 指向您的 schema 檔案
+  schema: "./drizzle/schema.ts",
   dialect: "mysql",
   dbCredentials: {
+    // 這裡我們直接從環境變數抓網址
     url: process.env.DATABASE_URL!,
   },
 });
