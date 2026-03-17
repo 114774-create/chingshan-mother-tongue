f50a34064f2ad34d8e351bd1128d62905b021df5
114774-create <114774@csps.tn.edu.tw>
2026-03-17 10:07:50 +0800

    Update trpc.ts

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,12 +1,10 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { createHTTPHandler } from "@trpc/server/adapters/node-http";
 import { appRouter } from "./routers.js";
 import { createContext } from "./context.js";
 
-export default async function handler(req: Request) {
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req,
-    router: appRouter,
-    createContext: () => createContext({ req } as any),
-  });
-}
+const handler = createHTTPHandler({
+  router: appRouter,
+  createContext,
+});
+
+export default handler;
